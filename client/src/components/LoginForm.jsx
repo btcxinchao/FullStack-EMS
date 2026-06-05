d9aa43be3dfa91cbf687c800c74d0681df910801
btcxinchao <buitanchuongg@gmail.com>
2026-06-05 15:12:02 +0700

    Verify Git identity

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,7 +1,21 @@
+import LoginLeftSide from "./LoginLeftSide"
+import {Link} from "react-router-dom"
+import {ArrowLeftIcon} from "lucide-react"
 
-const LoginForm = () => {
+const LoginForm = ({role,title,subtitle}) => {
   return (
-    <div>LoginForm</div>
+    <div className="min-h-screen flex flex-col md:flex-row  ">
+      <LoginLeftSide/>
+      <div className="w-full max-w-md animate-fade-in">
+        <Link to="/login" className="inline-flex items-center gap-2 text-slate-400 hover:text-slate-700 text-sm mb-10 transition-colors " >
+          <ArrowLeftIcon size={16} /> <p>Back to portals</p>
+        </Link>
+        <div className="mb-8">
+          <h1 className="text-xxl">{title}</h1>
+          <p>{subtitle}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
