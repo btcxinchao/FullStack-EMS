da7434269ec3ed4a261657370e2b524c86a5ad4d
btcxinchao <buitanchuongg@gmail.com>
2026-06-07 09:38:59 +0700

    Update feature

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,11 +1,24 @@
 import LoginLeftSide from "./LoginLeftSide"
 import {Link} from "react-router-dom"
-import {ArrowLeftIcon} from "lucide-react"
+import {ArrowLeftIcon, EyeIcon, EyeOffIcon, LoaderIcon} from "lucide-react"
+import { useState } from "react"
 
 const LoginForm = ({role,title,subtitle}) => {
+
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [showPassword,setShowPassword] = useState(false);
+  const [error,setError] = useState("");
+  const [loading,setLoading] = useState(false);
+
+  const handleSubmit = async (e) =>{
+          e.preventDefault();
+  }
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row  ">
       <LoginLeftSide/>
+      <div className="flex-1 flex items-center justify-center p-6 sm:p-12 bg-white">
       <div className="w-full max-w-md animate-fade-in">
         <Link to="/login" className="inline-flex items-center gap-2 text-slate-400 hover:text-slate-700 text-sm mb-10 transition-colors " >
           <ArrowLeftIcon size={16} /> <p>Back to portals</p>
@@ -14,6 +27,50 @@ const LoginForm = ({role,title,subtitle}) => {
           <h1 className="text-xxl">{title}</h1>
           <p>{subtitle}</p>
         </div>
+        {error && (<div className="mb-6 p-4 bg-rose-50 border border-rose-200 text-rose-200 text-rose-700 text-sm 
+                rounded-xl flex items-start gap-3">
+          <div className="w-1.5 h1.5 rounded-full bg-rose-500 mt-1.5 shrink-0">
+          </div>
+          {error}
+        </div>)}
+
+        <form action="" className="space-y-5" onSubmit={handleSubmit}>
+          {/* email */}
+          <div>
+            <label className="block text-sm font-medium text-slate-700 mb-2 " htmlFor="email">Email Adress</label>
+           
+            <input className="" type="email" id="email" onChange={(e) => setEmail(e.target.value)} 
+            required placeholder="john@example.com" />
+          </div>
+
+          {/* password */}
+          <div>
+            <label className="block text-sm font-medium text-slate-700 mb-2 " htmlFor="password">Password</label>
+             <div className="relative">
+            <input className="pr-11" type={showPassword ? "text" : "password"} id="password" 
+            onChange={(e) => setPassword(e.target.value)} 
+            required placeholder="......." />
+            <button type="button" className="absolute right-3 top-1/2 
+            -translate-y-1/2 text-slate-400 hover:text-slate-600 transition-colors"
+            onClick={()=>setShowPassword(!showPassword)}>
+              {showPassword ? <EyeOffIcon size={18}/> : <EyeIcon size={18}/>}
+            </button>
+            </div>
+          </div>
+          
+            <button type="submit"
+            disabled={loading}
+            className="w-full py-3 bg-linear-to-r from-indigo-600 to-indigo-500 text-white rounded-md
+            text-sm font-semibold hover:from-indigo-700 hover:to-indigo-600  disabled:opacity-50 transition-all
+            shadow-lg shadow-indigo-500/25 active:scale-[0.98] flex items-center justify-center"
+            > 
+             {loading && <LoaderIcon className="animate-spin h-4 w-4 mr-2"/>}
+            Sign In</button>
+          
+        </form>
+
+
+      </div>
       </div>
     </div>
   )
