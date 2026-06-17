40a5da2887816953c9bc29ffa80ec034d9a3738d
btcxinchao <buitanchuongg@gmail.com>
2026-06-17 10:36:36 +0700

    basic express app created

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log(" Database Connected");
+    });
+
+    await mongoose.connect(process.env.MONGODB_URL);
+
+  } catch (error) {
+    console.log(" Database connection failed:", error.message);
+  
+  }
+};
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import multer from "multer"
+import connectDB from "./config/db.js"
+
+const app = express()
+const port = 3000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(multer().none())
+
+await connectDB()
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
